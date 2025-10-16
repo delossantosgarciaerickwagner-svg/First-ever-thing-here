3f5227593a5a332d9683613ffbd9f905c85d9aec
delossantosgarciaerickwagner-svg <delossantosgarciaerickwagner@gmail.com>
2025-10-15 22:19:26 -0400

    Add health status evaluation in main.py
    first code in GitHub (15,10,25)

---
+++ main.py
@@ -0,0 +1,11 @@
+    health = 15
+    status = "none"
+
+>>> if health >=75 and health <=100:
+...     status = "hight HP"
+... if health >=30 and health <=74:
+...     status = "Take Care"
+... else:
+...     status = "Need Medkit"
+...
+>>> print(status)
